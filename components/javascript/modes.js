5e9538f1abc53c9017a853d23ab7fd768d7b05f6
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-06-27 16:27:15 +0400

    dark mode function is add

--- components/javascript/modes.js
+++ components/javascript/modes.js
@@ -1,1 +1,26 @@
-// clear yet
+const CHANGE_BUTTON = document.getElementById("handle_cont");
+const darkMode = localStorage.getItem("darkmode");
+
+const pageModes = () => {
+  const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+  };
+
+  const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+
+    localStorage.setItem("darkmode", null);
+  };
+
+  if (localStorage.getItem("darkmode") === "active") {
+    enableDarkMode();
+  }
+
+  CHANGE_BUTTON.addEventListener("click", () => {
+    const currentMode = localStorage.getItem("darkmode");
+    currentMode !== "active" ? enableDarkMode() : disableDarkMode();
+  });
+};
+
+pageModes();
