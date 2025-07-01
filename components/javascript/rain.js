0f0558a2bf132ff4f45bd70d75acd803e88966cb
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-07-01 20:59:14 +0400

    snow and rai effect bugs are fix

--- components/javascript/rain.js
+++ components/javascript/rain.js
@@ -1,15 +1,19 @@
+const contentWrapper = document.getElementsByClassName('content_wrapper');
+
 const crateRainDrop = () => {
     const rain_drop = document.createElement('i');
     rain_drop.classList.add('fas');
     rain_drop.classList.add('fa-droplet');
-    rain_drop.style.left = Math.random() * window.innerWidth + 'px';
-    rain_drop.style.animationDuration = Math.random() * 3 + 2 + 's';
+    // rain_drop.style.left = Math.random() * window.innerWidth + 'px';
+    rain_drop.style.left = Math.random() * contentWrapper[0].clientWidth + 'px';
+    rain_drop.style.animationDuration = Math.random() * 1 + 1 + 's';
     rain_drop.style.opacity = Math.random();
     rain_drop.style.fontSize = Math.random() * 3 + 3 +'px';
     
     
     
-    document.body.appendChild(rain_drop);
+    contentWrapper[0].appendChild(rain_drop);
+    // document.body.appendChild(rain_drop);
     
     setTimeout(() =>{
         rain_drop.remove();
--- components/javascript/snow.js
+++ components/javascript/snow.js
@@ -1,18 +1,20 @@
+const contentWrapper = document.getElementsByClassName('content_wrapper');
+
 const crateSnowFlake = () => {
     const snow_flake = document.createElement('i');
     snow_flake.classList.add('fas');
     snow_flake.classList.add('fa-snowflake');
-    snow_flake.style.left = Math.random() * window.innerWidth + 'px';
-    snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's';
+    snow_flake.style.left = Math.random() * contentWrapper[0].clientWidth + 'px';
+    snow_flake.style.animationDuration = Math.random() * 5 + 4 + 's';
     snow_flake.style.opacity = Math.random();
-    snow_flake.style.fontSize = Math.random() * 5 + 5 +'px';
+    snow_flake.style.fontSize = Math.random() * 4 + 4 +'px';
     
     
     
-    document.body.appendChild(snow_flake);
+    contentWrapper[0].appendChild(snow_flake);
     
     setTimeout(() =>{
         snow_flake.remove();
-    }, 5000)
+    }, 10000)
 }
 setInterval(crateSnowFlake, 50);
