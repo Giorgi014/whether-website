a5f48fc2929a452424a9d92c231bff30760c76b7
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-07-07 01:15:05 +0400

    fixed problem: weatherIcons.js has been removed and rewritten into app.js because it made problems in hosting

--- components/javascript/app.js
+++ components/javascript/app.js
@@ -61,7 +61,22 @@ const updateWeatherIcon = (iconPath, targetElement) => {
     targetElement.src = iconPath;
   }
 };
+const getWeatherIcon = (weatherCondition) => {
+  const condition = weatherCondition.toLowerCase();
 
+  const iconMap = {
+    clear: "./assets/icon/weather-icons-master/svg/wi-day-sunny.svg",
+    clouds: "./assets/icon/weather-icons-master/svg/wi-cloudy.svg",
+    rain: "./assets/icon/weather-icons-master/svg/wi-rain.svg",
+    drizzle: "./assets/icon/weather-icons-master/svg/wi-sprinkle.svg",
+    thunderstorm: "./assets/icon/weather-icons-master/svg/wi-thunderstorm.svg",
+    snow: "./assets/icon/weather-icons-master/svg/wi-snow.svg",
+    sleet: "./assets/icon/weather-icons-master/svg/wi-sleet.svg",
+    wind: "./assets/icon/weather-icons-master/svg/wi-strong-wind.svg",
+  };
+
+  return iconMap[condition];
+};
 const renderTodayWeather = (cities) => {
   const cityName = `${cities.city.name},`;
   const countryCode = cities.city.country;
